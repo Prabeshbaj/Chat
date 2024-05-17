1211f57fc5f507fad5064a3eae0f98823465f550
prabesh <prabesh@prabeshs-MBP.attlocal.net>
2024-05-16 21:46:24 -0400

    type

--- src/typewriter/typeWriter.tsx
+++ src/typewriter/typeWriter.tsx
@@ -4,17 +4,18 @@ const useTypewriter = (text: string, speed: number = 50) => {
   const [displayedText, setDisplayedText] = useState('');
 
   useEffect(() => {
+    setDisplayedText(''); // Reset displayed text before starting typing effect
     let currentIndex = 0;
     const intervalId = setInterval(() => {
       if (currentIndex < text.length) {
-        setDisplayedText(prev => prev + text[currentIndex]);
+        setDisplayedText(prev => prev + text.charAt(currentIndex));
         currentIndex += 1;
       } else {
         clearInterval(intervalId);
       }
     }, speed);
 
-    return () => clearInterval(intervalId);
+    return () => clearInterval(intervalId); // Cleanup interval on unmount
   }, [text, speed]);
 
   return displayedText;
